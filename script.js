6e1c119048b0692f7ea377ff389eaafd12a90dae
Tyler <36804325+misctyler@users.noreply.github.com>
2023-03-20 14:32:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const cornAddress = "0xa0c45509036c422ea7c4d4fcac26a9925531d8c3"
 const popCornAddress = "0x6531547b44784dDD8A934fB9fEB92ba582dfeD15"
 const popCornMachine = "0x1193d3f5d97e9a8a3B4511a718Eda88C21722B44"
+const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+const betAddress = "0xa4bfb9ae4999a97c5f93bEE59E4D126C904989aD"
+
 async function setup() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
     const provider = new ethers.providers.Web3Provider(window.ethereum)
 
     const signer = provider.getSigner()
 
-    const corn = new ethers.Contract(
-        cornAddress,
+    const usdc = new ethers.Contract(
+        usdcAddress,
         [
             "function approve(address, uint) external",
         ],
@@ -21,7 +24,7 @@ async function setup() {
         ],
         signer
     )
-    return { signer, corn, bet }
+    return { signer, usdc, bet }
 }
 
 async function connect(){
@@ -29,9 +32,9 @@ async function connect(){
 }
 
 async function approve() {
-    const { corn } = await setup()
+    const { usdc } = await setup()
     try {
-        await corn.approve(popCornMachine, "8000000000000000000000")
+        await usdc.approve(betAddress, "1000000000000")
     } catch (e) { alert(e) }
 }
 
