f3e3752a2f22cfc433c498821ae36ca2fba5207e
MiscTyler <MikeTylerMosier@gmail.com>
2023-04-01 11:36:11 -0400

    error log & validator

--- contracts/goldKernelValidator.sol
+++ contracts/goldKernelValidator.sol
@@ -2,16 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract GoldTokenValidator is ERC721Holder {
     
-    address constant GOLD_TOKEN_CONTRACT = 0xDBB09CEd27B571885A1B4EBd093587eDC00eae20;
-    uint8 constant GOLD_TOKEN_TRAIT_INDEX = 0;
-    string constant GOLD_TOKEN_TRAIT_VALUE = "Gold";
+    address constant GOLD_TOKEN_CONTRACT    = 0xDBB09CEd27B571885A1B4EBd093587eDC00eae20;
+    uint8   constant GOLD_TOKEN_TRAIT_INDEX = 0;
+    string  constant GOLD_TOKEN_TRAIT_VALUE = "Gold";
     
     function validateGoldTokenOwnership(address _owner) external view returns(bool) {
-        ERC721 goldToken = ERC721(GOLD_TOKEN_CONTRACT);
+        ERC721Enumerable goldToken = ERC721Enumerable(GOLD_TOKEN_CONTRACT);
         uint256 tokenCount = goldToken.balanceOf(_owner);
         for (uint256 i = 0; i < tokenCount; i++) {
             uint256 tokenId = goldToken.tokenOfOwnerByIndex(_owner, i);
