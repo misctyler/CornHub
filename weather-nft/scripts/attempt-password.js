5cf406e23a71cf870d6448fb64fcd742d6148988
MiscTyler <MikeTylerMosier@gmail.com>
2023-06-24 11:31:20 -0500

    cleanup ;

--- weather-nft/scripts/attempt-password.js
+++
@@ -1,6 +0,0 @@
-// truffle console --network matic
-// const weatherNFT = await WeatherNFT.deployed()
-// await weatherNFT.attemptPassword("FreeBe")
-// Make sure it has some link ;)
-
-// Use the console!
